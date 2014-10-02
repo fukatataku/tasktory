95df5007ca467e21840c4fe516f2dc60e2444f94
fukatataku <fukatataku@gmail.com>
2014-10-02 09:57:48 +0900

    update RWTemplate

--- lib/common/RWTemplate.py
+++ lib/common/RWTemplate.py
@@ -2,7 +2,6 @@
 
 import re
 from string import Template
-from functools import reduce
 
 class RWTemplate(Template):
     """読み書きテンプレート
@@ -11,45 +10,39 @@ class RWTemplate(Template):
     tmpl.parse('2014年4月1日') -> {'YEAR':'2014', 'MONTH':'4', 'DAY':'1'}
     tmpl.parse('年4月1日') -> {'YEAR':'', 'MONTH':'4', 'DAY':'1'}
     tmpl.substitute({'YEAR':'2014', 'MONTH':'4', 'DAY':'1'}) -> '2014年4月1日'
+    ※ デリミタの変更は禁止
     """
 
     delimiter = '%'
 
-    __reg_id = re.compile(r'({0}*)({0}{{?([A-Za-z_][A-Za-z0-9_]*)}}?)'\
-                          .format(delimiter))
+    __esc_reg = re.compile('([.^$*+?{}\\\[\]|()])')
+    __hold_reg = re.compile(r'%(\\\{)?([A-Za-z_][A-Za-z0-9_]*)(?(1)\\\})')
 
     def __init__(self, template):
         super().__init__(template)
-        self.__parse_template(template)
 
-    def __parse_template(self, tmpl):
-        tmp = [(p,i) for e,p,i in self.__reg_id.findall(tmpl) if len(e)%2==0]
-        holder, self.iD = zip(*tmp) if tmp else ((), ())
-        h_indice = RWTemplate.__indice(holder, tmpl)
-        c_indice = RWTemplate.__interspace(0, len(tmpl), h_indice)
-        self.caret = c_indice[0][1] > 0
-        self.dollar = c_indice[-1][0] < len(tmpl)
-        self.const = [tmpl[s:e].replace(self.delimiter*2, self.delimiter)
-                      for s,e in c_indice if not s==e]
+        pattern = template
+
+        # 特殊な文字をエスケープする
+        pattern = self.__esc_reg.sub(r'\\\1', template)
+
+        # %% をを退避する
+        pattern_list = pattern.split('%%')
+
+        # プレースホルダーを正規表現に変換する
+        pattern_list = [self.__hold_reg.sub(r'(?P<\2>.*?)', s)
+                for s in pattern_list]
+
+        # % を復帰させる
+        pattern = '%'.join(pattern_list)
+
+        # 行頭に \A, 行末に \Z を追加する
+        self.pattern = r'\A' + pattern + r'\Z'
+
+        # コンパイルする
+        self.tmpl_reg = re.compile(self.pattern, re.DOTALL)
 
     def parse(self, string):
-        c_indice = RWTemplate.__indice(self.const, string)
-        v_indice = RWTemplate.__interspace(0, len(string), c_indice)
-        values = [string[s:e] for s,e in v_indice]
-        if self.caret: values.pop(0)
-        if self.dollar: values.pop(-1)
-        return dict(zip(self.iD, values))
-
-    @staticmethod
-    def __indice(char_list, string):
-        ret, last = [], 0
-        for c in char_list:
-            index = string.index(c, last)
-            last = index + len(c)
-            ret.append((index, last))
-        return ret
-
-    @staticmethod
-    def __interspace(start, end, indice):
-        return reduce(lambda x,y:x[0:-1] + [(x[-1][0], y[0]),
-                      (y[-1], x[-1][-1])], indice, [(start, end)])
+        m = self.tmpl_reg.match(string)
+        if not m: raise ValueError()
+        return m.groupdict()
